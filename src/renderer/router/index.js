8ec4929ad5b7cf4cc9d0bbb54a55314de6a3e613
Terry Cai <gidcai@gmail.com>
2019-10-12 02:27:45 +0800

    fixed vue router error

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -3,6 +3,12 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch((err) => err)
+}
+
 const content = () => import('../page/content')
 const about = () => import('../page/about')
 const suggestion = () => import('../page/suggestion')
