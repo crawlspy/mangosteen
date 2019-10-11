8125f75a5a9296dd613b83059cf7e652da6f3168
Terry Cai <gidcai@gmail.com>
2019-10-11 17:04:19 +0800

    更新壁纸设置

--- src/get-image/bing.js
+++ src/get-image/bing.js
@@ -1,65 +1,40 @@
 /** *
  * bing网站 https://www.pexels.com
  */
-
-const axios = require('axios')
-const cheerio = require('cheerio')
-const { imageMinWidth, browserHeader } = require('../utils/config')
-
-const { CancelToken } = axios
+import fetch from 'electron-fetch'
 
 let source = null
 
-// 下一页的时间参数
-let nextPageSeed = ''
 export const getImage = function (data) {
+    console.log(data)
     return new Promise((resolve, reject) => {
         if (!data) {
             resolve([])
+            return
         }
-        if (data.page === 0){
-            nextPageSeed = ''
+        if (data.page > 0) {
+            resolve([])
+            return
         }
-        let baseUrl = 'https://www.pexels.com'
-        if (data.searchKey) {
-            baseUrl = `https://www.pexels.com/search/${data.searchKey}`
-        }
-        source = CancelToken.source()
-        const url = baseUrl + nextPageSeed
-        axios.get(url, {
-            cancelToken: source.token,
-            headers: browserHeader,
-        }).then((result) => {
-            source = null
-            const urls = []
-            const $ = cheerio.load(result.data)
-            if ($('.next_page').length){
-                nextPageSeed = $('.next_page')[0].attribs.href
-            }
-            const imageItems = $('.photo-item__img')
-            // 是否存在
-            if (nextPageSeed.length){
-                const imageItemsLength = imageItems.length
-                for (let i = 0; i < imageItemsLength; i++){
-                    const { attribs } = imageItems[i]
-                    const obj = {
-                        width: attribs['data-image-width'],
-                        height: attribs['data-image-height'],
-                        url: attribs['data-large-src'],
-                        downloadUrl: attribs['data-big-src'].split('?')[0]
+        const request = fetch('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8')
+        source = request
+            .then(res => res.json())
+            .then((json) => {
+                let urls = []
+                urls = json.images.map((item) => {
+                    return {
+                        url: `https://www.bing.com${item.url}`,
+                        downloadUrl: `https://www.bing.com${item.url}`,
+                        width: '1920',
+                        height: '1080'
                     }
-                    // 剔除重复的地址
-                    if (parseInt(obj.width, 10) > imageMinWidth) {
-                        urls.push(obj)
-                    }
-                }
-            }
-            resolve(urls)
-        }).catch((err) => {
-            source = null
-            console.log('------------请求失败pexels:', url)
-            reject()
-        })
+                })
+                resolve(urls)
+            }).catch((err) => {
+                source = null
+                console.log('------------请求失败bing:')
+                reject()
+            })
     })
 }
 
