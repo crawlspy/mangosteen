53e4683cbe71a53225a13eee11c58df153f5272f
Terry Cai <gidcai@gmail.com>
2019-10-12 02:30:36 +0800

    update 500px  source

--- src/get-image/500px.js
+++ src/get-image/500px.js
@@ -10,60 +10,43 @@
 // 热门
 // https: //api.500px.com/v1/photos?rpp=50&feature=popular&image_size%5B%5D=1&image_size%5B%5D=2&image_size%5B%5D=32&image_size%5B%5D=31&image_size%5B%5D=33&image_size%5B%5D=34&image_size%5B%5D=35&image_size%5B%5D=36&image_size%5B%5D=2048&image_size%5B%5D=4&image_size%5B%5D=14&sort=&include_states=true&include_licensing=true&formats=jpeg%2Clytro&only=&exclude=&personalized_categories=&page=1&rpp=50
 
-
-const axios = require('axios')
-const cheerio = require('cheerio')
-
-const { axiosGet } = require('../utils/axios')
+import fetch from 'electron-fetch'
+import queryString from 'query-string'
 
 const { imageMinWidth } = require('../utils/config')
 
-const { CancelToken } = axios
 let source = null
-
-let cookies = ''
-let csrToken = ''
-let htmlAddress = ''
 
 /**
  * 先获取页面取得cookie等信息
  * @param {*} data 
  */
-function getHtmlPage(data) {
-    return new Promise((resolve, reject) => {
-        if (data.searchKey) {
-            htmlAddress = `https://500px.com/search?submit=%E6%8F%90%E4%BA%A4&q=${data.searchKey}&type=photos`
-        } else {
-            htmlAddress = 'https://500px.com/popular'
-        }
-        axios.get(htmlAddress).then((result) => {
-            cookies = result.headers['set-cookie']
-            const $ = cheerio.load(result.data)
-            csrToken = $('meta[name="csrf-token"]')[0].attribs.content
-            resolve()
-        }).catch((error) => {
-            console.log('-------------------500px获取cookie出错')
-            reject(error)
-        })
-    })
-}
+
 
 export const getImage = async function (data) {
-    if (cookies === '') {
-        await getHtmlPage(data)
-    }
+    // eslint-disable-next-line no-async-promise-executor
     return new Promise(async (resolve, reject) => {
         if (!data) {
             resolve([])
         }
         let baseUrl = 'https://api.500px.com/v1/photos'
         const params = {
-            image_size: [100, 200, 400, 600, 2048, 4096, 5120, 6144, 7168],
-            page: data.page,
+            'image_size[]': 2048,
+            // image_size: [100, 200, 400, 600, 2048],
+            page: data.page + 1,
             rpp: 50, // 单页条数
-            formats: 'jpeg'
+            formats: 'jpeg,lytro',
+            sort: '',
+            exclude: '',
+            personalized_categories: ''
         }
+        
+
         if (data.searchKey) {
+            // baseUrl = 'https://api.500px.com/v1/photos'
+            // params.feature = 'popular'
+            // params.include_licensing = true
+            // params.include_states = true
             baseUrl = 'https://api.500px.com/v1/photos/search'
             params.type = 'photos'
             params.term = data.searchKey
@@ -75,53 +58,55 @@ export const getImage = async function (data) {
             params.include_licensing = true
             params.include_states = true
         }
-        source = CancelToken.source()
-        axiosGet({
-            url: baseUrl,
-            params,
-            headers: {
-                Accept: 'application/json, text/javascript, */* q=0.01',
-                Origin: 'https://500px.com',
-                Referer: htmlAddress,
-                'User-Agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-                'X-CSRF-Token': csrToken,
-                cookie: cookies
-            },
-            cancelToken: source.token
-        }).then((result) => {
-            source = null
-            const urls = []
-            const { photos } = result
-            photos.forEach((item) => {
-                const obj = {
-                    width: item.width,
-                    height: item.height,
-                    url: '',
-                    downloadUrl: result,
-                }
-                const { images } = item
-                let maxSize = 0
-                for (let i = 0; i < images.length; i++) {
-                    if (images[i].size >= 200 && images[i].size <= 700) {
-                        obj.url = images[i].https_url
+
+        const str = queryString.stringify(params)
+
+        const request = fetch(`${baseUrl}?${str}`)
+        source = request
+            .then((res) => res.json())
+            .then((json) => {
+                const urls = []
+
+                const { photos } = json
+                photos.forEach((item) => {
+                    const obj = {
+                        width: item.width,
+                        height: item.height,
+                        url: item.image_url[0],
+                        downloadUrl: item.images[0].url,
                     }
-                    if (images[i].size > maxSize) {
-                        obj.downloadUrl = images[i].https_url
+                    const { images } = item
+                    let maxSize = 0
+                    for (let i = 0; i < images.length; i++) {
+                        if (images[i].size >= 200 && images[i].size <= 700) {
+                            obj.url = images[i].https_url
+                        }
+                        if (images[i].size > maxSize) {
+                            obj.downloadUrl = images[i].https_url
+                        }
+                        maxSize = images[i].size
                     }
-                    maxSize = images[i].size
-                }
-                // obj.height = parseInt(maxSize / obj.width * obj.height, 10)
-                // obj.width = maxSize
-                if (parseInt(obj.width, 10) > imageMinWidth){
-                    urls.push(obj)
-                }
+                    // obj.height = parseInt(maxSize / obj.width * obj.height, 10)
+                    // obj.width = maxSize
+                    if (parseInt(obj.width, 10) > imageMinWidth){
+                        urls.push(obj)
+                    }
+                })
+                // urls = json.photos.map((item) => {
+                //     console.log(item.images)
+                //     return {
+                //         url: item.image_url[0],
+                //         downloadUrl: item.images[0].url,
+                //         width: item.width,
+                //         height: item.height
+                //     }
+                // })
+                resolve(urls)
+            }).catch((err) => {
+                source = null
+                console.log('------------请求失败500px:', err)
+                reject()
             })
-            resolve(urls)
-        }).catch(() => {
-            source = null
-            console.log('------------请求失败500px:', baseUrl)
-            reject()
-        })
     })
 }
 
