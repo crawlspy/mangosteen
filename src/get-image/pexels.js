44dfae807898ef83fc64b87a2ef9d06483f48e51
Terry Cai <gidcai@gmail.com>
2019-10-15 17:04:02 +0800

    修复pexels错误

--- src/get-image/pexels.js
+++ src/get-image/pexels.js
@@ -1,12 +1,10 @@
 /** *
  * pexels网站 https://www.pexels.com
  */
+import fetch from 'electron-fetch'
 
-const axios = require('axios')
 const cheerio = require('cheerio')
-const { imageMinWidth, browserHeader } = require('../utils/config')
-
-const { CancelToken } = axios
+const { imageMinWidth } = require('../utils/config')
 
 let source = null
 
@@ -24,48 +22,49 @@ export const getImage = function (data) {
         if (data.searchKey) {
             baseUrl = `https://www.pexels.com/search/${data.searchKey}`
         }
-        source = CancelToken.source()
         const url = baseUrl + nextPageSeed
-        axios.get(url, {
-            cancelToken: source.token,
-            headers: browserHeader,
-        }).then((result) => {
-            source = null
-            const urls = []
-            const $ = cheerio.load(result.data)
-            if ($('.next_page').length){
-                nextPageSeed = $('.next_page')[0].attribs.href
-            }
-            const imageItems = $('.photo-item__img')
-            // 是否存在
-            if (nextPageSeed.length){
-                const imageItemsLength = imageItems.length
-                for (let i = 0; i < imageItemsLength; i++){
-                    const { attribs } = imageItems[i]
-                    const obj = {
-                        width: attribs['data-image-width'],
-                        height: attribs['data-image-height'],
-                        url: attribs['data-large-src'],
-                        downloadUrl: attribs['data-big-src'].split('?')[0]
-                    }
-                    // 剔除重复的地址
-                    if (parseInt(obj.width, 10) > imageMinWidth) {
-                        urls.push(obj)
+        const request = fetch(url)
+        request
+            .then((res) => res.text())
+            .then((html) => {
+                // console.log(html)
+                const urls = []
+                const $ = cheerio.load(html)
+                if ($('.next_page').length){
+                    nextPageSeed = $('.next_page')[0].attribs.href
+                }
+                const imageItems = $('.photo-item__img')
+                // 是否存在
+                if (nextPageSeed.length){
+                    const imageItemsLength = imageItems.length
+                    for (let i = 0; i < imageItemsLength; i++){
+                        const { attribs } = imageItems[i]
+                        if (attribs['data-big-src']) {
+                            const obj = {
+                                width: attribs['data-image-width'],
+                                height: attribs['data-image-height'],
+                                url: attribs['data-large-src'],
+                                downloadUrl: attribs['data-big-src'].split('?')[0]
+                            }
+                            // 剔除重复的地址
+                            if (parseInt(obj.width, 10) > imageMinWidth) {
+                                urls.push(obj)
+                            }
+                        }
                     }
                 }
-            }
-            resolve(urls)
-        }).catch((err) => {
-            source = null
-            console.log('------------请求失败pexels:', url)
-            reject()
-        })
+                resolve(urls)
+            }).catch((err) => {
+                source = null
+                console.log('------------请求失败pexels:', err)
+                reject()
+            })
     })
 }
 
 
 export const cancelImage = function () {
     if (source) {
-        source.cancel()
+        // source.cancel()
     }
 }
